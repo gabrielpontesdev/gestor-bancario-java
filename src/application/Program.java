c8a2d8813321455237eaf112a45cc1c890cf0efa
Gabriel <gabrielpontes.Tech@gmail.com>
2026-01-20 14:58:05 -0300

    implementa a classe Account com construtores sobrecarregados e lógica de depósito inicial

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,44 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Account account;
+		
+		System.out.print("Enter account number: ");
+		int accountNumber = sc.nextInt();
+		
+		sc.nextLine(); // Limpeza de buffer (importante!)
+		
+		
+		System.out.print("Enter account number: ");
+		String holder = sc.nextLine();
+		
+		System.out.print("Is there an initial deposit (y/n)? ");
+		char response = sc.next().toLowerCase().charAt(0);
+		
+		if (response == 'y') {
+		    System.out.print("Enter initial deposit value: ");
+		    double initialDeposit = sc.nextDouble();
+		    // Aqui você usa o seu Construtor com Sobrecarga (3 parâmetros)
+		    account = new Account(holder, accountNumber, initialDeposit);
+			}	
+		else {
+		    // Aqui você usa o Construtor sem o depósito
+			account = new Account(holder, accountNumber);
+		}
 
+		
+		
+		sc.close();
 	}
 
 }
---
+++ src/entities/Account.java
@@ -0,0 +1,50 @@
+package entities;
+
+public class Account {
+	
+	private String holder;
+	private int accountNumber;
+	private double balance;
+	public double initialDeposit;
+	public Account() {
+		
+	}
+	
+	public Account(String holder, int accountNumber) {
+		this.holder = holder;
+		this.accountNumber = accountNumber;
+	}
+
+	public Account(String holder, int accountNumber, double initialDeposit) {
+		this.holder = holder;
+		this.accountNumber = accountNumber;
+		//Saldo começa com o depósito lido, se optar por realizar
+		deposit(initialDeposit);
+	}
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	
+	public void deposit(double amount) {
+	    if (amount > 0) {
+	        this.balance += amount;
+	    }
+	}
+	
+	
+	
+	
+
+}
